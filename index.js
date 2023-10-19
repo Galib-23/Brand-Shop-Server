3e99b8f101f60de2e0ac8e0260817fa3bfeb6ae4
Galib <aagalib2323@gmail.com>
2023-10-20 03:17:12 +0600

    uinque product get method created

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogz7mxs.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -28,6 +28,13 @@ async function run() {
     app.get('/products', async(req, res) => {
       const cursor = productCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query);
       res.send(result);
     })
 
